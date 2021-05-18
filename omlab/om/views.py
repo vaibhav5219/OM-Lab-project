212c13aaa5865908e26e9407b108911f6896f3ec
vaibhav5219 <vaibhavkumar5219@gmail.com>
2021-05-18 21:33:29 +0530

    Modified template and view
    template - removing unused tags
    views - removing unused functions

--- omlab/om/views.py
+++ omlab/om/views.py
@@ -17,17 +17,3 @@ def display(request):
         else:
             error="Enter a valid  name and passcode"
     return (render(request,"om/index.html",{"error":error}))
-
-def login(request):
-    if request.method=="POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request)
-            data = models.OmLab.objects.all()
-            return (render(request, "om/show_all.html",{'data':data}))
-        return(res)
-    res=render(request, "om/login_lab.html")
-    return(res)
-
